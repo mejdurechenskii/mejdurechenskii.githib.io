7ab9cc7cf032507cc1cc9b4745e53b018c5bd07f
mejdurechenskii <50547232+mejdurechenskii@users.noreply.github.com>
2019-05-28 19:29:49 +0700

    add updater

---
+++ precache-manifest.fd81d050a23499ff0bf807e81e3f6eec.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "571e30628c7ad07d748a",
+    "url": "./static/js/main.571e3062.chunk.js"
+  },
+  {
+    "revision": "715b6c8c5fbdbad4af15",
+    "url": "./static/js/1.715b6c8c.chunk.js"
+  },
+  {
+    "revision": "38f067787582820bb05a2aeba27a9460",
+    "url": "./index.html"
+  }
+];
